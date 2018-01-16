5582f7eb352fd448ac7adde4b14c0b59128782cc
abequinonez <abequinonez@users.noreply.github.com>
2018-01-15 22:51:38 -0800

    refactor: Move deck-specific functions to deck object

--- js/app.js
+++ js/app.js
@@ -1,70 +1,99 @@
-/*
-Create an array containing each card type, represented by a Font Awesome class
-name. A pair of cards will be created for each card type in the forEach()
-method that follows. The missing 'fa-' prefix will be added to each card when
-its HTML is created.
-*/
-let cardList = [
-    'diamond',
-    'paper-plane-o',
-    'anchor',
-    'bolt',
-    'cube',
-    'leaf',
-    'bicycle',
-    'bomb'
-];
+// Create an object that contains properties specific to the card deck
+const deck = {
+    // Set up the card deck
+    setup() {
+        const cardList = this.createCardList();
+        this.displayCards(cardList);
+        this.addEventListeners();
+    },
+    // Create the card list
+    createCardList() {
+        /*
+        Create an array containing each card type, represented by a Font
+        Awesome class name. A pair of cards will be created for each card type
+        in the forEach() method that follows. The missing 'fa-' prefix will be
+        added to each card when its HTML is created.
+        */
+        let cardList = [
+            'diamond',
+            'paper-plane-o',
+            'anchor',
+            'bolt',
+            'cube',
+            'leaf',
+            'bicycle',
+            'bomb'
+        ];
 
-// Add a copy of each card type to the cardList array to create pairs of cards
-cardList.forEach(function(card) {
-    cardList.push(card);
-});
+        /*
+        Add a copy of each card type to the cardList array to create pairs
+        of cards.
+        */
+        cardList.forEach(function(card) {
+            cardList.push(card);
+        });
 
-// Display the cards on the page
-function displayCards() {
-    // Shuffle the list of cards
-    cardList = shuffle(cardList);
+        return cardList;
+    },
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
 
-    // Create a fragment to reduce reflow and repaint
-    const fragment = document.createDocumentFragment();
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
 
-    // Loop through each card and create its HTML
-    for (const card of cardList) {
-        // Create a list item element for each card
-        const cardElem = document.createElement('li');
-        cardElem.classList.add('card');
+        return array;
+    },
+    // Display the cards on the page
+    displayCards(cardList) {
+        // Shuffle the list of cards
+        cardList = this.shuffle(cardList);
 
-        // Create a string variable containing each card's HTML text
-        const cardHTML = `<i class="fa fa-${card}"></i>`;
+        // Create a fragment to reduce reflow and repaint
+        const fragment = document.createDocumentFragment();
 
-        // Parse each card's text as HTML and insert it into the card element
-        cardElem.insertAdjacentHTML('beforeend', cardHTML);
+        // Loop through each card and create its HTML
+        for (const card of cardList) {
+            // Create a list item element for each card
+            const cardElem = document.createElement('li');
+            cardElem.classList.add('card');
 
-        // Append each card element to the document fragment
-        fragment.appendChild(cardElem);
+            // Create a string variable containing each card's HTML text
+            const cardHTML = `<i class="fa fa-${card}"></i>`;
+
+            /*
+            Parse each card's text as HTML and insert it into the
+            card element.
+            */
+            cardElem.insertAdjacentHTML('beforeend', cardHTML);
+
+            // Append each card element to the document fragment
+            fragment.appendChild(cardElem);
+        }
+
+        // Select the deck element
+        const deckElem = document.querySelector('.deck');
+
+        // Append the document fragment to the deck element
+        deckElem.appendChild(fragment);
+    },
+    // Add a click event listener to each card
+    addEventListeners() {
+        const cards = document.getElementsByClassName('card');
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].addEventListener('click', function() {
+                console.log(this);
+                console.log(this.firstElementChild.classList[1]);
+                this.classList.add('open', 'show');
+            });
+        }
     }
-
-    // Select the deck element
-    const deck = document.querySelector('.deck');
-
-    // Append the document fragment to the deck element
-    deck.appendChild(fragment);
-}
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
+};
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -77,13 +106,4 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-displayCards();
-
- const cards = document.getElementsByClassName('card');
- for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function() {
-        console.log(this);
-        console.log(this.firstElementChild.classList[1]);
-        this.classList.add('open', 'show');
-    });
- }
+deck.setup();
